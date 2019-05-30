8405c62634d0509861c0a912a3341954cb23095b
marcpre <marcus.presich@gmail.com>
2019-05-30 09:07:56 +0200

    css-fix and fixing app

--- cmp-databinding/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -34,7 +34,7 @@ export class CockpitComponent implements OnInit {
     })
   }
 
-  onBlueprintAdded() {
+  onAddBlueprint() {
     this.blueprintCreated.emit( {
       serverName: this.newServerName,
       serverContent: this.newServerContent
