ec003a563b0f6dfbc49143af4faf1ea3d0653b84
marcpre <marcus.presich@gmail.com>
2019-08-10 07:02:51 +0200

    set value vs patch value

--- README.md
+++ README.md
@@ -47,4 +47,4 @@ Used to emit data.
 
 
 
----> Stopped at 13/187
+---> Stopped at 13/188
--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -14,6 +14,20 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+/*     this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: '',
+      questionAnswer: '',
+      gender: 'male'
+    }) */
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+    })
   }
 
 /*   onSubmit(form: NgForm) {
