cbf9b56c2d4460e9d86c96502ce0cc97dab83955
marcpre <marcus.presich@gmail.com>
2019-09-12 06:47:24 +0200

    send request to firebase

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServerService } from './servers.service';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,9 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+  
+  constructor(private serverService: ServerService) {}
+  
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -25,6 +29,14 @@ export class AppComponent {
       id: this.generateId()
     });
   }
+
+  onSave() {
+    this.serverService.storeServers(this.servers).subscribe(
+        (response) => console.log(response), 
+        (error) => console.log(error)
+      );
+  }
+
   private generateId() {
     return Math.round(Math.random() * 10000);
   }
